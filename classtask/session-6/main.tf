bc629c2fc408fecbec185da77de515ac7b2c2992
Gulnazzholshy01 <gulnaz.zholshy@yandex.kz>
2025-01-06 16:50:25 -0600

    old

---
+++ classtask/session-6/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  vpc_security_group_ids = [aws_security_group.web.id]
+  instance_type = var.env == "prod" ? var.instance_type[1] : var.instance_type[0]
+  user_data              = file("./extras/userdata.sh")
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(
+    {Name = format(local.Name, "ec2")},
+    local.common_tags
+  )
+}
+
+
