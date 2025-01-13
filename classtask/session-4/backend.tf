4a5590486d69fcfceb3c068be7cd10cd43d4ffd4
Mohammadzarifi <iqbalzarifi#@icloud.com>
2025-01-12 20:35:36 -0600

    session-4 watched 2:02

--- classtask/session-4/backend.tf
+++ classtask/session-4/backend.tf
@@ -1,13 +1,7 @@
 terraform {
   backend "s3" {
-<<<<<<< HEAD
     bucket = "for-class"
     key    = "classtasks/session-4/terraform.tfstate"
     region = "us-west-2"
-=======
-    bucket = "terraform-aug24-123456789"
-    key    = "classtasks/session-4/terraform.tfstate"
-    region = "us-east-1"
->>>>>>> b051ce993f04500cdf3ea28bf5275c173dcbbe3f
   }
 }
--- classtask/session-4/variables.tf
+++ classtask/session-4/variables.tf
@@ -1,21 +1,12 @@
 variable "instance_type" {
-<<<<<<< HEAD
   type        = string
   default     = "t2.micro"
-=======
-  type = string
-    default = "t2.micro"
->>>>>>> b051ce993f04500cdf3ea28bf5275c173dcbbe3f
   description = "This is Instance Type"
 }
 
 variable "instance_name" {
   type        = string
-<<<<<<< HEAD
   default     = "my"
-=======
-  default     = "My-Webserver"
->>>>>>> b051ce993f04500cdf3ea28bf5275c173dcbbe3f
   description = "This is My Webserver"
 }
 
@@ -25,7 +16,6 @@ variable "security_group_name" {
   description = "This is My Security Group"
 }
 
-<<<<<<< HEAD
 
 variable "env" {
   type        = list(string)
@@ -43,22 +33,3 @@ variable "env" {
 #   default     = ["0.0.0.0/0", "73.74.183.123/32", "0.0.0.0/0", "0.0.0.0/0"]
 #   description = "List of CIDRs"
 # }
-=======
-variable "env" {
-  type        = list(string)
-  default     = ["dev", "qa", "prod", "stage"]
-  description = "This is My Dev Environment"
-}
-
-variable "ports" {
-  type = list(number)
-  default = [22, 80, 443, 3369]
-  description = "List of ports"
-}
-
-variable "cidrs" {
-  type = list(string)
-  default = ["0.0.0.0/0","73.74.183.123/32","0.0.0.0/0","0.0.0.0/0"]
-  description = "List of ports"
-}
->>>>>>> b051ce993f04500cdf3ea28bf5275c173dcbbe3f
