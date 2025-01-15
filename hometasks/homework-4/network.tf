33082c7f0deb081ed6c0162e998c5a440e170d4d
Mohammadzarifi <iqbalzarifi@icloud.com>
2025-01-15 14:01:51 -0600

    new push connecting repo to the comp

--- hometasks/homework-4/network.tf
+++ hometasks/homework-4/network.tf
@@ -19,55 +19,7 @@ resource "aws_subnet" "private_subnet" {
 
 
 
-# # Create Internet Gateway
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.my_vpc.id
-#   tags = {
-#     Name = "my_igw"
-#   }
-# }
-
-# # Allocate Elastic IP for NAT Gateway
-# resource "aws_eip" "nat_eip" {
-#   domain = "vpc"
-# }
-
-# # Create NAT Gateway
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat_eip.id
-#   subnet_id     = aws_subnet.public_subnet[0].id
-#   tags = {
-#     Name = "my_nat_gateway"
-#   }
-# }
-
-# # Create Route Tables
-# resource "aws_route_table" "public" {
-#   vpc_id = aws_vpc.my_vpc.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
-
-#   tags = {
-#     Name = "public_route_table"
-#   }
-# }
-
-# resource "aws_route_table" "private" {
-#   vpc_id = aws_vpc.my_vpc.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat.id
-#   }
-
-#   tags = {
-#     Name = "private_route_table"
-#   }
-# }
-
+/*
 # # Associate Public Subnets with Public Route Table
 # resource "aws_route_table_association" "public_subnet_assoc" {
 #   count          = 3
@@ -81,3 +33,4 @@ resource "aws_subnet" "private_subnet" {
 #   subnet_id      = aws_subnet.private_subnet[count.index].id
 #   route_table_id = aws_route_table.private.id
 # }
+*/
